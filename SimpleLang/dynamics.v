a03ef445425bd14f606b1818de44d8ba36533f4d
dangerstone <stinnadanger@gmail.com>
2020-10-28 21:45:50 +0100

    Defined subst-function

--- SimpleLang/dynamics.v
+++ SimpleLang/dynamics.v
@@ -1,21 +1,50 @@
 From SimpleLang Require Export statics.
 
+Require Import Coq.Arith.PeanoNat.
+
 (* SUBSTITUTION *)
+(** helper function: shift *)
+Fixpoint shift (i j : nat) (e : expr) : expr :=
+e. (* NB!: e is just a placeholder so I could define subst with shift *)
+(*match e with
+  | unit => unit
+  TODO continue...
+end.*)
+
 (** in expression e substitute variable i with s *)
 Fixpoint subst (e : expr) (i : id) (s : expr) : expr :=
   match e with
-  | Unit => Unit
-  (*TODO continue...*)
+  | unit => unit
+  | Var n => if (i =? n) then s else (if (n <? i) then (Var n) else (Var (n-1)))
+
+  (* numbers *)
+  | Nat n => Nat n
+  | add e1 e2 => add (subst e1 i s) (subst e2 i s)
+  | sub e1 e2 => sub (subst e1 i s) (subst e2 i s)
+  | mul e1 e2 => mul (subst e1 i s) (subst e2 i s)
+  | le e1 e2 => le (subst e1 i s) (subst e2 i s)
+  | lt e1 e2 => lt (subst e1 i s) (subst e2 i s)
+  | eq e1 e2 => eq (subst e1 i s) (subst e2 i s)
+
+  (* booleans *)
+  | Bool b => Bool b
+  | ifthenelse e1 e2 e3 => ifthenelse (subst e1 i s) (subst e2 i s) (subst e3 i s)
+
+  (* products *)
+  | pair e1 e2 => pair (subst e1 i s) (subst e2 i s)
+  | fst e1 => fst (subst e1 i s)
+  | snd e1 => snd (subst e1 i s)
+
+  (* sums *)
+  | inj1 e1 => inj1 (subst e1 i s)
+  | inj2 e1 => inj2 (subst e1 i s)
+  | matchwith e1 e2 e3 => matchwith (subst e1 i s) (subst e2 (i+1) (shift 0 1 s)) (subst e3 (i+1) (shift 0 1 s))
+
+  (* recursive functions *)
+  | rec e1 => rec (subst e1 (i+2) (shift 0 2 s))
+  | app e1 e2 => app (subst e1 i s) (subst e2 i s)
   end
 .
-
-(** helper function: shift *)
-Fixpoint shift (i j : nat) (e : expr) : expr :=
-match e with
-  | Unit => Unit
-  (*TODO continue*)
-end.
-
 
 
 (* OPERATIONAL SEMANTICS *)
